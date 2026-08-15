04aa9a6e525d021e454a11e369ea4625faaa0c79
sushii40 <bandgarsnehal01@gmail.com>
2026-08-15 14:47:42 +0530

    Test Docker CI/CD pipeline

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AutoScale Hub is running!"
+    return "AutoScale Hub v2 - CI/CD is working!"
 
 @app.route("/health")
 def health():
@@ -13,4 +13,3 @@ def health():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
-    
